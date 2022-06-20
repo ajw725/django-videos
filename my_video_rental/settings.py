aaf34b56d22d0eb1d9804710d1fd02afd41861f0
Andrew Weinstein <andrew.weinstein@invitae.com>
2022-06-20 12:30:48 -0600

    override admin base_site template

--- my_video_rental/settings.py
+++ my_video_rental/settings.py
@@ -53,10 +53,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'my_video_rental.urls'
 
+TEMPLATE_DIR = BASE_DIR / 'templates'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
